1d405015ae6ab01b6cbef1329d588e0887e59826
youngchangyoo <ddori5338@hanmail.net>
2024-02-12 20:09:52 +0900

    made guess number game

--- main.js
+++ main.js
@@ -12,9 +12,23 @@ let answer = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
+let upCat = document.getElementsByClassName("up-cat")[0];
+let downCat = document.getElementsByClassName("down-cat")[0];
+let punchCat = document.getElementsByClassName("punch-cat")[0];
+let spaceCat = document.getElementsByClassName("space-cat")[0];
 
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("mousedown", function () {
+    userInput.value = "";
+});
+
 
 function pickRandomNumber() {
     answer = Math.floor(Math.random() * 100) + 1;
@@ -23,13 +37,64 @@ function pickRandomNumber() {
 
 function play() {
     let userValue = userInput.value;
+    if (userValue < 1 || userValue > 100) {
+        resultArea.textContent = "1과 100사이 숫자를 입력해 주세요";
+        return;
+    }
+    if (history.includes(userValue)) {
+        resultArea.textContent = "이전에 입력했던 숫자입니다";
+        return;
+    }
+    chances--;
+    chanceArea.textContent = `남은 기회: ${chances}번`;
     if (userValue < answer) {
-        resultArea.textContent = 'up!';
+        resultArea.textContent = 'UP!';
+        upCat.style.display = "block";
+        downCat.style.display = "none";
     } else if (userValue > answer) {
-        resultArea.textContent = 'down!';
+        resultArea.textContent = 'DOWN!';
+        upCat.style.display = "none";
+        downCat.style.display = "block";
     } else {
         resultArea.textContent = 'correct!';
+        gameOver = true;
     }
+    history.push(userValue);
+
+    if (chances <= 0) {
+        gameOver = true;
+    }
+    if (gameOver) {
+        playButton.disabled = true;
+        upCat.style.display = "none";
+        downCat.style.display = "none";
+        if (chances <= 0) {
+            resultArea.textContent = '실패!';
+            resultArea.style.color = "red";
+            punchCat.style.display = "block";
+        } else {
+            resultArea.textContent = '성공!';
+            resultArea.style.color = "rgb(106, 255, 0)";
+            spaceCat.style.display = "block";
+        }
+    }
+}
+
+function reset() {
+    // user input 창 정리, 새로운 번호 생성
+    userInput.value = "";
+    pickRandomNumber();
+    resultArea.textContent = '여기에 결과가 나온다';
+    chances = 5;
+    chanceArea.textContent = `남은 기회: ${chances}번`;
+    playButton.disabled = false;
+    gameOver = false;
+    history = [];
+    upCat.style.display = "none";
+    downCat.style.display = "none";
+    punchCat.style.display = "none";
+    spaceCat.style.display = "none";
+    resultArea.style.color = "white";
 }
 
 pickRandomNumber();
